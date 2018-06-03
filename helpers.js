95fdb8bba644bdc4c5646e5ae64832c4d9097a0d
unknown <junaid@homepass.com>
2018-06-03 19:38:20 +1000

    tetttsss

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 var request = require("then-request");
 
-const createTestRun = () =>
+const createTestRun = (testRunName) =>
   request("POST", "https://app.hiptest.com/api/projects/86205/test_runs", {
     headers: {
       accept: "application/vnd.api+json; version=1",
@@ -8,12 +8,12 @@ const createTestRun = () =>
       uid: "jur550@gmail.com",
       client: "gXzotQpPxAa7VEIKpHG2GQ"
     },
-    json: { data: { attributes: { name: "Sprint3" } } }
+    json: { data: { attributes: { name: testRunName } } }
   });
 
-const createTestRun2 = testRunId =>
+const getAllTests = testRunId =>
   request(
-    "POST",
+    "GET",
     `https://app.hiptest.com/api/projects/86205/test_runs/${testRunId}/test_snapshots`,
     {
       headers: {
@@ -21,17 +21,46 @@ const createTestRun2 = testRunId =>
         "access-token": "Toi8ENseYIl4tdWaddLwlg",
         uid: "jur550@gmail.com",
         client: "gXzotQpPxAa7VEIKpHG2GQ"
-      },
-      json: { data: { attributes: { name: "Sprint3" } } }
+      }
     }
   );
 
-async function createNewTestRun() {
-  const res = await createTestRun();
+  const postTestResult = (testRunId, testSnapshotId, testStatus) =>
+  request(
+    "Post",
+    `https://app.hiptest.com/api/projects/86205/test_runs/${testRunId}/test_snapshots/${testSnapshotId}/test_results`,
+    {
+      headers: {
+        accept: "application/vnd.api+json; version=1",
+        "access-token": "Toi8ENseYIl4tdWaddLwlg",
+        uid: "jur550@gmail.com",
+        client: "gXzotQpPxAa7VEIKpHG2GQ"
+      },
+      json: {data: {type: "test-results", attributes: {status: testStatus, "status-author": "detox", description: "All was well"}}}
+
+    }
+  );
+
+async function createNewTestRun(name) {
+  const res = await createTestRun(name);
   const body = JSON.parse(res.getBody().toString("utf8"));
-  return body;
+  return body.data.id;
+}
+
+async function getTests(testRunId) {
+  const res = await getAllTests(testRunId);
+  const body = JSON.parse(res.getBody().toString("utf8"));
+  return body.data;
+}
+
+async function addTestExecResult(testRunId, testSnapshotId, testStatus) {
+  const res = await postTestResult(testRunId, testSnapshotId, testStatus);
+  const body = JSON.parse(res.getBody().toString("utf8"));
+  return body.data;
 }
 
 module.exports = {
-  createNewTestRun
+  createNewTestRun,
+  getTests,
+  addTestExecResult
 };
--- test/test.js
+++ test/test.js
@@ -3,22 +3,42 @@ var request = require("then-request");
 const call = require("../helpers");
 
 describe("Array", function() {
-  before(async function() {});
+  let tests; 
+  let testRunId = '163856'; 
+  before(async function() {
+    // const testRunId = await call.createNewTestRun('sp 4');
+     tests = await call.getTests('163856'); 
+
+    tests.forEach(element => {
+      console.log(element.attributes.name)
+    });
+    // console.log(JSON.stringify(tests, undefined, 2))
+
+  });
 
   afterEach(async function() {
     console.log(this.currentTest.state);
-    console.log(typeof this.currentTest.title);
-    if (this.currentTest.state != "passed") {
-      console.log("hey my tests failed");
-      console.log("hey my tests failed");
-      // const res = await call.makeApiCall();
-      // console.log(res);
-    } else {
-      console.log("hey my tests passed");
+    const currentTestName = this.currentTest.title; 
+
+    const currentHipTest = tests.filter(function(el){
+      return el.attributes.name === currentTestName
+    });
+
+    if (currentHipTest[0].id){
+    const testss = await call.addTestExecResult(testRunId, currentHipTest[0].id, this.currentTest.state)
+    console.log(`=======>>>>  ${testss}`)
     }
   });
 
-  it("should return -1 when the value is not present", async function() {
+  it("s2", async function() {
     assert.equal([1, 2, 3].indexOf(4), -5);
+  });
+
+  it("s1", async function() {
+    assert.equal([1, 2, 3].indexOf(4), -1);
+  });
+
+  it("should not return -3 when the value is not present", async function() {
+    assert.equal([1, 2, 3].indexOf(4), -3);
   });
 });
